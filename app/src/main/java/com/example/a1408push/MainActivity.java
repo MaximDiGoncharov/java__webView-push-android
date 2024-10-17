4af9fe047479d5ba135842480e825d0b19510d5b
MaximDiGoncharov <maxpups101@gmail.com>
2024-10-17 16:19:45 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/a1408push/MainActivity.java
+++ app/src/main/java/com/example/a1408push/MainActivity.java
@@ -2,62 +2,95 @@ package com.example.a1408push;
 
 import static androidx.fragment.app.FragmentManager.TAG;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.ClipData;
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.content.ClipboardManager;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.ContactsContract;
 import android.util.Log;
+import android.view.View;
 import android.webkit.ConsoleMessage;
 import android.webkit.CookieManager;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import android.widget.TextView;
+import android.widget.Button;
 
-import com.example.a1408push.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.messaging.FirebaseMessaging;
 
-import java.util.HashMap;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int NOTIFICATION_PERMISSION_CODE = 1;
+
     private static final int FILECHOOSER_RESULTCODE = 2; // Код для выбора файла
-    private static final int REQUEST_CODE = 100; // Код для запроса разрешений
+    private static final int NOTIFICATION_PERMISSION_CODE = 1;
+    private static final int REQUEST_CODE = 100;
     public static String WebViewCookies;
+    private static final int PICK_CONTACT = 1;
     private WebView webView;
     private ValueCallback<Uri[]> mFilePathCallback;
+
+
 
     @SuppressLint({"SetJavaScriptEnabled", "RestrictedApi"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getWindow().getDecorView().setBackgroundColor(Color.BLACK);
 
-        // Запрос разрешений на доступ к файлам
+        getWindow().getDecorView().setBackgroundColor(Color.BLACK);
         requestFilePermissions();
 
-        WebView myWebView = findViewById(R.id.webView);
+
+        // Проверяем версию Android
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestNotificationPermission();
+        } else {
+            // Для более старых версий не нужно запрашивать разрешение
+//            sendNotification();
+        }
+        WebView myWebView = (WebView) findViewById(R.id.webView);
+
         myWebView.setWebChromeClient(new WebChromeClient() {
+            public boolean onConsoleMessage() {
+                return onConsoleMessage((ConsoleMessage) null);
+            }
             @Override
             public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
-                mFilePathCallback = filePathCallback;
+                ValueCallback<Uri[]> mFilePathCallback = filePathCallback;
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
                 intent.setType("*/*");
                 startActivityForResult(Intent.createChooser(intent, "Выберите файл"), FILECHOOSER_RESULTCODE);
                 return true;
             }
-
             public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
                 Handler handler = new Handler();
                 System.out.println(consoleMessage.message());
@@ -86,18 +119,25 @@ public class MainActivity extends AppCompatActivity {
                         return;
                     }
                     String token = task.getResult();
-                    Log.d(TAG, token);
-                    myWebView.loadUrl("https://universal.laxo.one/enter/" + token, new HashMap<String, String>() {{
-                        put("Cache-Control", "no-cache");
-                    }});
+                    Log.d(TAG,token);
+                    //myAwesomeTextView.setText(token);
+                    requestNotificationPermission();
+                    myWebView.loadUrl("https://universal.laxo.one/enter/" + token );
+                    // myWebView.loadUrl("https://contact-picker.glitch.me/");
                 });
         FirebaseMessaging.getInstance().subscribeToTopic("web_app")
-                .addOnCompleteListener(task -> {
-                    String msg = "Done";
-                    if (!task.isSuccessful()) {
-                        msg = "Failed";
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = "Done";
+                        if (!task.isSuccessful()) {
+                            msg = "Failed";
+                        }
+
                     }
                 });
+
+
     }
 
     private void requestFilePermissions() {
@@ -127,6 +167,17 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+// Запрос разрешения на уведомления
+    private void requestNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
+            }
+        }
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -137,13 +188,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void requestNotificationPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
-                    != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.POST_NOTIFICATIONS}, NOTIFICATION_PERMISSION_CODE);
-            }
-        }
-    }
+
+
+
 }
