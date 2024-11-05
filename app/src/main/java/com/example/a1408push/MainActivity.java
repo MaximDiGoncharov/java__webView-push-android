0ebf58e2c0276aa3478b233624006ef6508419dd
MaximDiGoncharov <maxpups101@gmail.com>
2024-11-05 17:22:35 +0300

    Update MainActivity.java
    Добавил необходимые разрешения для загрузки файлов!

--- app/src/main/java/com/example/a1408push/MainActivity.java
+++ app/src/main/java/com/example/a1408push/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.a1408push;
 
 import static androidx.fragment.app.FragmentManager.TAG;
 
+import android.app.DownloadManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
 import android.content.ClipData;
 import android.Manifest;
 import android.content.Intent;
@@ -12,11 +14,16 @@ import android.database.Cursor;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.ContextCompat;
+
 import android.content.ClipboardManager;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -34,11 +41,16 @@ import android.util.Log;
 import android.view.View;
 import android.webkit.ConsoleMessage;
 import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.URLUtil;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -47,20 +59,29 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    private ValueCallback<Uri[]> uploadMessage;
+    private static final int FILECHOOSER_RESULTCODE = 1;
 
+    static int IS_PUSH_LINK = 0;
     private static final int NOTIFICATION_PERMISSION_CODE = 1;
+    private static final int REQUEST_CODE = 100;
     public static String WebViewCookies;
     private static final int PICK_CONTACT = 1;
-    private WebView webView;
-
+    @SuppressLint("StaticFieldLeak")
+    public static WebView webView;
+    private ValueCallback<Uri[]> mFilePathCallback;
 
     @SuppressLint({"SetJavaScriptEnabled", "RestrictedApi"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webView);
 
         getWindow().getDecorView().setBackgroundColor(Color.BLACK);
+        requestFilePermissions();
+
+
         // Проверяем версию Android
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             requestNotificationPermission();
@@ -68,26 +89,8 @@ public class MainActivity extends AppCompatActivity {
             // Для более старых версий не нужно запрашивать разрешение
 //            sendNotification();
         }
-        WebView myWebView = (WebView) findViewById(R.id.webView);
-//        TextView myAwesomeTextView = (TextView)findViewById(R.id.myAwesomeTextView);
-//        View copyButton = findViewById(R.id.copyButton);
-//        copyButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                copyTextToClipboard(myAwesomeTextView.getText().toString());
-//            }
-//        });
-        myWebView.setWebChromeClient(new WebChromeClient() {
-            public boolean onConsoleMessage() {
-                return onConsoleMessage((ConsoleMessage) null);
-            }
 
-            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
-                Handler handler = new Handler();
-                System.out.println(consoleMessage.message());
-                return true;
-            }
-        });
+
 
         CookieManager.getInstance().setAcceptThirdPartyCookies(myWebView, true);
         WebSettings webSettings = myWebView.getSettings();
@@ -100,6 +103,25 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setDisplayZoomControls(false);
         webSettings.setSupportZoom(true);
         webSettings.setDefaultTextEncodingName("utf-8");
+        myWebView.setWebChromeClient(new WebChromeClient() {
+            // Метод для Android 5.0+
+            @Override
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+                uploadMessage = filePathCallback;
+
+                // Создаем интент для выбора файла
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("*/*"); // Можно указать конкретный тип файла, если нужно
+                startActivityForResult(Intent.createChooser(intent, "Выберите файл"), FILECHOOSER_RESULTCODE);
+
+                return true; // Возвращаем true, чтобы сообщить, что мы обработали запрос
+            }
+
+        });
+//        myWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+//        myWebView.clearCache(true);
+
         FirebaseMessaging.getInstance().getToken()
                 .addOnCompleteListener(task -> {
                     if (!task.isSuccessful()) {
@@ -107,12 +129,15 @@ public class MainActivity extends AppCompatActivity {
                         return;
                     }
                     String token = task.getResult();
-
-                    Log.d(TAG,token);
-                    //myAwesomeTextView.setText(token);
+                    Log.d(TAG, token);
                     requestNotificationPermission();
-                    myWebView.loadUrl("https://universal.laxo.one/enter/" + token );
-                    // myWebView.loadUrl("https://contact-picker.glitch.me/");
+                    myWebView.loadUrl("https://universal.laxo.one/enter/" + token);
+//                                myWebView.loadUrl("https://universal.laxo.one/web/" + token);
+
+//                    myWebView.loadUrl("javascript:" + "window.location.href = 'https://yandex.com/';");
+                    //     myWebView.evaluateJavascript("window.location.href = 'https://yandex.com/';", null);
+
+
                 });
         FirebaseMessaging.getInstance().subscribeToTopic("web_app")
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
@@ -128,50 +153,52 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-//    private void copyTextToClipboard(String text) {
-//        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-//        ClipData clip = ClipData.newPlainText("Copied Text", text);
-//        clipboard.setPrimaryClip(clip);
-//    }
-//    }
-// Запрос разрешения на уведомления
-private void requestNotificationPermission() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
-                != PackageManager.PERMISSION_GRANTED) {
+
+    private void requestFilePermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_CODE);
         }
     }
-}
-
-//    // Метод для открытия Contact Picker
-//    public void pickContact() {
-//        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-//        startActivityForResult(intent, PICK_CONTACT);
-//    }
-//
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (requestCode == PICK_CONTACT && resultCode == RESULT_OK) {
-//            Uri contactUri = data.getData();
-//            String[] projection = {ContactsContract.Contacts.DISPLAY_NAME};
-//
-//            Cursor cursor = getContentResolver().query(contactUri, projection, null, null, null);
-//            if (cursor != null && cursor.moveToFirst()) {
-//                String contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-//                cursor.close();
-//
-//                // Теперь вы можете передать имя контакта обратно в WebView или обработать его по своему усмотрению
-//                webView.evaluateJavascript("javascript:handleContactName('" + contactName + "')", null);
-//            }
-//        }
-//    }
-//
 
 
 
+
+
+// Запрос разрешения на уведомления
+    private void requestNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            } else {
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == FILECHOOSER_RESULTCODE) {
+            if (uploadMessage == null) return;
+            Uri result = data == null || resultCode != RESULT_OK ? null : data.getData();
+
+            // Возвращаем результат в uploadMessage
+            uploadMessage.onReceiveValue(result != null ? new Uri[]{result} : null);
+            uploadMessage = null; // Обнуляем uploadMessage после обработки
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
 }
